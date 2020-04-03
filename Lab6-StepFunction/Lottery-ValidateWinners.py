aec8f4435dad1bf48353fee522c597525e69480f
ruiliang <ruiliang@amazon.com>
2020-04-03 12:35:21 +0800

    align Readme code and source code

--- Lab6-StepFunction/Lottery-ValidateWinners.py
+++ Lab6-StepFunction/Lottery-ValidateWinners.py
@@ -21,11 +21,20 @@ def lambda_handler(event, context):
     
     # if winner is in the past draw, return 0 else return 1
     has_winner_in_queue = 1 if len(output) > 0 else 0
+    
+    # format the winner details in sns
+    winner_names = [winner['employee_name'] for winner in winner_details]
+    
+    name_s = ""
+    for name in winner_names:
+        name_s += name
+        name_s += " "
+    
     return {
         "body": {
             "num_of_winners": num_of_winners,
             "winner_details": winner_details
         },
-        "status": has_winner_in_queue
+        "status": has_winner_in_queue,
+        "sns": "Congrats! [{}] You have selected as the Lucky Champ!".format(name_s.strip())
     }
-        
